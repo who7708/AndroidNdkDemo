700260411672ac8349138b67e7e6ff7803007f46
Chris <who7708@163.com>
2021-02-11 21:31:03 +0800

    修复jstring转换成cString

--- app/src/main/cpp/jni/jni_string.cpp
+++ app/src/main/cpp/jni/jni_string.cpp
@@ -7,15 +7,19 @@
 extern "C"
 JNIEXPORT jstring JNICALL
 Java_com_example_ndk_jni_JniString_callNativeString(JNIEnv *env, jobject thiz, jstring _str) {
-    const char *str = env->GetStringUTFChars(_str, nullptr);
+    const char *str = env->GetStringUTFChars(_str, JNI_FALSE);
     LOGE("java string is %s", str);
-    env->ReleaseStringChars(_str, reinterpret_cast<const jchar *>(str));
+    env->ReleaseStringUTFChars(_str, str);
     return env->NewStringUTF("我是被修改过的字符串");
 }
 
 
 extern "C"
 JNIEXPORT void JNICALL
-Java_com_example_ndk_jni_JniString_stringMethod(JNIEnv *env, jobject thiz, jstring str) {
-    // TODO: implement stringMethod()
+Java_com_example_ndk_jni_JniString_stringMethod(JNIEnv *env, jobject thiz, jstring _str) {
+    const char *str = env->GetStringUTFChars(_str, JNI_FALSE);
+    LOGE("java string is %s", str);
+
+    env->ReleaseStringUTFChars(_str, str);
+
 }
